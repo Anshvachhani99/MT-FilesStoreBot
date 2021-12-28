099ae17e9f0c66b2569af4d07a35c24705a29f2b
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-28 16:41:37 +0530

    Update check_user_status.py

--- plugins/check_user_status.py
+++ plugins/check_user_status.py
@@ -4,7 +4,7 @@ import datetime
 from config import LOG_CHANNEL
 from plugins.database import Database
 
-db = Database(form Config import DB_URL)
+db = Database(config.DB_URL)
 
 async def handle_user_status(bot, cmd):
     chat_id = cmd.from_user.id
