20e1a2f8b5adc44c753ef232e8760471bb56d26f
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-28 16:18:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,8 +25,8 @@ class Bot(Client):
             "Bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            mo_tech_yt={
-                "root": "mo_tech_yt"
+            plugins={
+                "root": "plugins
             },
             workers=TG_BOT_WORKERS,
             bot_token=TG_BOT_TOKEN
