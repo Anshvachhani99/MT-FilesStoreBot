8597455907bb4be5898f55d9d0372358bb772eb2
Anshvachhani99 <92299607+Anshvachhani99@users.noreply.github.com>
2021-12-28 16:17:53 +0530

    Add files via upload

---
+++ plugins/check_user_status.py
@@ -0,0 +1,30 @@
+# (c) @MRK_YT
+
+import datetime
+from configs import LOG_CHANNEL
+from mo_tech_yt.database import Database
+
+db = Database(Config.DATABASE_URL)
+
+async def handle_user_status(bot, cmd):
+    chat_id = cmd.from_user.id
+    if not await db.is_user_exist(chat_id):
+        total=await client.get_chat_members_count(message.chat.id)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_G.format(message.chat.title, message.chat.id, total, "Unknown"))     
+        await db.add_chat(message.chat.id, message.chat.title)
+        return 
+    if not await db.is_user_exist(message.from_user.id):
+        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
+
+
+    ban_status = await db.get_ban_status(chat_id)
+    if ban_status["is_banned"]:
+        if (
+                datetime.date.today() - datetime.date.fromisoformat(ban_status["banned_on"])
+        ).days > ban_status["ban_duration"]:
+            await db.remove_ban(chat_id)
+        else:
+            await cmd.reply_text("You are Banned to Use This Bot 🥺", quote=True)
+            return
+    await cmd.continue_propagation()
